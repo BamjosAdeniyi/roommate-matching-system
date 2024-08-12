51f1b65bc6f3874d98da74271f9c86f704100094
Oluwabamise Adeniyi <133222521+BamjosAdeniyi@users.noreply.github.com>
2024-08-12 21:18:00 +0100

    Added an instruction to takepersonality test on the result page

--- frontend/app/user/results.php
+++ frontend/app/user/results.php
@@ -37,5 +37,7 @@ include '../../../backend/assessment/process_result.php';
     </ul>
 
     <a href="/roommate-matching-system/frontend/app/user/user_dashboard.html"><button>Back to Dashboard</button></a>
+    <p>If the personality trait parameters are empty, It means you have not taken the personality assessment test yet. Use the personality assessment button on your <a href="/roommate-matching-system/frontend/app/user/user_dashboard.html">dashboard</a> to take the personality assessment test.</p>
+    
 </body>
 </html>
